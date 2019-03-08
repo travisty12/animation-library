1411d0a426f8901a278f685904dbf8ebcfb936c3
Student <example@example.com>
2019-03-07 16:34:07 -0800

    add combinatorial triangle, text, start of explanatory animations

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function fact( i ) {
     return i * fact( i - 1 );
   }
 }
-function fib( n ) {
+function fib1( n ) {
   var number = 1;
   for ( var i = 1; i < ( Math.floor( ( n - 1 ) / 2 ) + 1 ); i++ ) {
     var product = 1;
@@ -14,6 +14,15 @@ function fib( n ) {
     }
     number += product / fact( i );
   }
+  return number;
+}
+
+function fib2( n ) {
+  var number = 0;
+  for ( var i = 1; i < Math.ceil( ( n / 2 ) + 1 ); i++ ) {
+    number += ( Math.pow(5, (i - 1)) * (fact(n) / ( fact(2*i - 1) * fact(n - (2*i - 1)))) )
+  }
+  number /= Math.pow(2, (n-1));
   return number;
 }
 
@@ -39,7 +48,7 @@ function buildTri(place,numRow) {
   for (var i = 0; i < $("#" + place)[0].childElementCount; i++) {
     row = "";
     for (var j = 0; j < i + 1; j++) {
-      if (place == "pascNum") {
+      if (place != "pascComb") {
         row += "<div id='" + place + "row" + i + "col" + j + "'><h1>" + ((fact(i)) / (fact(j) * fact(i - j))) + "</h1></div>";
       } else if (place == "pascComb") {
         row += "<div class='combElement' id='" + place + "row" + i + "col" + j + "'><h1 class='par'>(</h1><div class='comb'><h1>" + i + "</h1><h1>" + j + "</h1></div><h1 class='par'>)</h1></div>";
@@ -54,17 +63,51 @@ function buildTri(place,numRow) {
 
 
 $(document).ready(function() {
+  buildTri("example",3);
+  $("#example").fadeIn();
+  $("#example").css("display","flex");
   buildTri("pascNum",10);
   buildTri("pascComb",10)
   // $(".jelly").click(function() {
   //   getPos(this);
   //
   // });
+  $("#example").click(function() {
+    $(".explanation").fadeOut();
+    $(this).addClass("spinOut");
+    setTimeout(function() {
+      $("#example").css("display","none");
+      $(this).css("height","0");
+      $("#text1").fadeIn();
+      $("#text2").fadeIn();
+      $("#pascNum").css("display","flex");
+      $("#pascNum").css("top","0");
+      $("#pascNum").addClass("spinIn");
+      setTimeout(function() {
+        $("#pascNum").removeClass("spinIn");
+        // setTimeout(function() {
+        //   $("#pascNumrow0").append("<div id='line1'></div>");
+        //   $("#line1").css("position","relative");
+        //   $("#line1").css("left","20px");
+        //
+        // }, 1000);
+      }, 1000);
+
+    }, 1000);
+  });
   $("#pascNum").click(function() {
     $(this).addClass("spinOut");
     setTimeout(function() {
       $(this).hide();
       $(this).css("height","0");
+      $("#text1").fadeOut();
+      $("#text2").fadeOut();
+      setTimeout(function() {
+        $("#text3").fadeIn();
+        $("#text4").fadeIn();
+        $("#nck").fadeIn();
+      }, 400);
+      $("#nck").css("display","flex");
       $("#pascComb").css("display","flex");
       $("#pascComb").css("top","0");
       $("#pascComb").addClass("spinIn");
